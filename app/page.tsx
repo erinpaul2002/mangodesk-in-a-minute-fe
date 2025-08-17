fbaef4bf1e8414bed53be2f50cba04c510fb499c
erinpaul2002 <erinpaul121102@gmail.com>
2025-08-17 08:41:44 +0530

    added health check on mount to wake backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { useEffect } from 'react';
+
 import MeetingSummarizer from './components/MeetingSummarizer';
+import { healthCheck } from './apiClient';
 
 export default function MeetingSummarizerPage() {
+  useEffect(() => {
+    healthCheck().catch(() => {});
+  }, []);
   return <MeetingSummarizer />;
 }
